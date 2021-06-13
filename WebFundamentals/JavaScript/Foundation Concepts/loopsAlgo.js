92363d8e591a95d45e86238354cd8fe801f374e0
Charlie Bowman <c.b.bowman@outlook.com>
2021-06-13 14:46:54 -0400

    loops algo practice assignment

---
+++ WebFundamentals/JavaScript/Foundation Concepts/loopsAlgo.js
@@ -0,0 +1,14 @@
+/* Print odds 1-20 */
+
+for(var i=1; i<=20; i+=2) {
+    console.log(i);
+}
+
+/* Sum and Print 1-5 */
+
+var sum=0;
+for(var i=1; i<=5; i++) {
+    sum+=i;
+    console.log("Num: "+i);
+    console.log("Sum: "+sum);
+}
