098ccb05b22028cc6bf4421076b753c0a2836027
Charles Bowman <cbowman@hcps.us>
2021-06-23 14:12:35 -0400

    fibonacci function

--- WebFundamentals/JavaScript/More Practice/IntermediateFoundation.js
+++ WebFundamentals/JavaScript/More Practice/IntermediateFoundation.js
@@ -9,3 +9,28 @@ function sigma(num){
     }
     return output;
 }
+
+function factorial(num){
+    if(num<0){
+        console.log("parameter cannot be negative");
+        return("error");
+    }
+    var output=1;
+    for(var i=num; i>0; i--){
+        output*=i;
+    }
+    return output;
+}
+
+function fib(num){
+    if(num<0){
+        console.log("parameter cannot be negative");
+        return("error");
+    }
+    if(num<2){
+        return(num);
+    }
+    return fib(num-2)+fib(num-1);
+}
+
+console.log(fib(5));
