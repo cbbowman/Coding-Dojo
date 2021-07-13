41a0c229eec5fa646a8d0e9276d9be5d4c10766f
Charlie Bowman <c.b.bowman@outlook.com>
2021-07-12 22:00:11 -0400

    bank account assignment

--- Python_Stack/Python_Track/Python_OOP/bankAccount.py
+++ Python_Stack/Python_Track/Python_OOP/bankAccount.py
@@ -5,6 +5,8 @@ class BankAccount:
 	def deposit(self, amount=0):
 		# increases the account balance by the given amount
 		self.balance+=amount
+		return self
+
 	def withdraw(self, amount=0):
 		# decreases the account balance by the given amount if there are sufficient funds; if there is not enough money, print a message "Insufficient funds: Charging a $5 fee" and deduct $5
 		if self.balance<amount:
@@ -12,21 +14,21 @@ class BankAccount:
 			self.balance-=5
 		else:
 			self.balance-=amount
-		
+		return self
+
 	def display_account_info(self):
 		# print to the console: eg. "Balance: $100"
 		print("Balance: ", self.balance)
+		return self
+
 	def yield_interest(self):
 		# increases the account balance by the current balance * the interest rate (as long as the balance is positive)
 		if self.balance>0:
 			self.balance=round(self.balance*(1+self.int_rate), 2)
+		return self
 
-myAccount = BankAccount(0.17,100000)
+firstAccount = BankAccount(0.07,1000)
+secondAccount = BankAccount(0.17,100000)
 
-myAccount.display_account_info()
-myAccount.deposit(73)
-myAccount.display_account_info()
-myAccount.withdraw(13)
-myAccount.display_account_info()
-myAccount.yield_interest()
-myAccount.display_account_info()
+firstAccount.deposit(73).deposit(73).deposit(73).withdraw(13).yield_interest().display_account_info()
+secondAccount.deposit(73).deposit(73).withdraw(73).withdraw(73).withdraw(73).withdraw(13).yield_interest().display_account_info()
