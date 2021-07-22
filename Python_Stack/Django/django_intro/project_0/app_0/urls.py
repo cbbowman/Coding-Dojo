999cc04c1c6bc1c8b43ba6eef94ef7c5de0db9df
Charlie Bowman <c.b.bowman@outlook.com>
2021-07-22 19:39:43 -0400

    finished assignment

--- Python_Stack/Django/django_intro/project_0/app_0/urls.py
+++ Python_Stack/Django/django_intro/project_0/app_0/urls.py
@@ -2,4 +2,9 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index),
+    path('new', views.new),
+    path('create', views.create),
+    path('<int:number>', views.show),
+    path('<int:number>/edit', views.edit),
+    path('<int:number>/delete', views.destroy),
 ]
--- Python_Stack/Django/django_intro/project_0/app_0/views.py
+++ Python_Stack/Django/django_intro/project_0/app_0/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import redirect, render, HttpResponse
 
 def index(request):
     return HttpResponse("placeholder to later display a list of all blogs")
+
+def new(request):
+    return HttpResponse("placeholder to display a new form to create a new blog")
+
+def create(request):
+    return redirect('/')
+
+def show(request, number):
+    return HttpResponse(f"placeholder to display blog number: {number}")
+
+def edit(request, number):
+    return HttpResponse(f"placeholder to edit blog number: {number}")
+
+def destroy(request, number):
+    return redirect('/')
