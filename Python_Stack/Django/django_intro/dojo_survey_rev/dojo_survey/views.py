71221260897b725588feeb967ef054fce0724c42
Charlie Bowman <c.b.bowman@outlook.com>
2021-07-25 20:58:48 -0400

    dojo survey revisited

---
+++ Python_Stack/Django/django_intro/dojo_survey_rev/dojo_survey/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import redirect, render
+    
+def index(request):
+    context = {
+    }
+    return render(request,'index.html', context)
+
+def submit(request):
+	request.session['name']=request.POST['name']
+	request.session['location']=request.POST['location']
+	request.session['favLang']=request.POST['favLang']
+	request.session['comment']=request.POST['comment']
+	return redirect('/confirmation')
+
+def confirmation(request):
+    context = {
+    }
+    return render(request,'confirmation.html')
+
+# def result(request):
+# 	if request.method == "POST":
+# 		context = {
+# 			"name": request.POST ["name"],
+# 			"location": request.POST["location"],
+# 			"favLang": request.POST["favLang"],
+# 			"comment": request.POST["comment"]
+# 		}
+# 	return render(request,'result.html', context)
